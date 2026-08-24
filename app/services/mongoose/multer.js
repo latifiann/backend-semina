3c09dfb8b2725fec78b9b193f7c308aaf065b706
latifiann <latifian.iman44@gmail.com>
2026-08-24 12:23:48 +0700

    feat: add image upload API

---
+++ app/services/mongoose/multer.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+const { BadRequestError } = require("../../errors");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Math.floor(Math.random() * 99999999) + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new BadRequestError("Unsupported file format"));
+  }
+};
+
+const uploadMiddleware = multer({
+  storage,
+  limits: {
+    fileSize: 3000000,
+  },
+  fileFilter: fileFilter,
+});
+
+module.exports = uploadMiddleware;
