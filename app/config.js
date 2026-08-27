370829d7b3abeb695e33d467dff3ab6af3a990c9
latifiann <latifian.iman44@gmail.com>
2026-08-28 00:35:55 +0700

    feat: add JWT authentication API

--- app/config.js
+++ app/config.js
@@ -1,6 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET wajib diisi!");
+}
+
 module.exports = {
   urlDb: process.env.URL_MONGODB_DEV,
+  jwtExpiration: "30m",
+  jwtSecret: process.env.JWT_SECRET,
 };
